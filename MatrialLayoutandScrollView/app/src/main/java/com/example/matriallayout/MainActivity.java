9c43f314db85db61fe7e2acdda9dd91f02189dff
fajar098123 <fajar098123@gmail.com>
2021-03-26 17:07:22 +0700

    BKPM Minggu 2

---
+++ MatrialLayoutandScrollView/app/src/main/java/com/example/matriallayout/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.matriallayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.materialdesign);
+    }
+}
